8c9c7f373e17adaa37f9088cc3d076148731d3e2
stebeus <189894010+stebeus@users.noreply.github.com>
2026-04-22 08:18:44 -0300

    feat: add loadPage

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+import { readFileSync } from 'node:fs';
 import { createServer } from 'node:http';
 
 const server = createServer();
 
 const notFoundPage = 'pages/404';
+
+function loadPage(response, path = notFoundPage) {
+  const success = 200;
+  const notFound = 400;
+
+  response.writeHead(path === notFound ? notFound : success, {
+    'Content-Type': 'text/html',
+  });
+
+  const html = readFileSync(`${path}.html`);
+  response.end(html);
+}
