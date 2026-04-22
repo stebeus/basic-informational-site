ab45705e75f3e5a9055d85f115ba6de070561adb
stebeus <189894010+stebeus@users.noreply.github.com>
2026-04-22 08:21:39 -0300

    feat: route server request pages

--- index.js
+++ index.js
@@ -16,3 +16,23 @@ function loadPage(response, path = notFoundPage) {
   const html = readFileSync(`${path}.html`);
   response.end(html);
 }
+
+server.on('request', ({ url }, response) => {
+  if (url === '/') {
+    loadPage(response, 'index');
+    return;
+  }
+
+  if (url === 'about') {
+    loadPage(response, 'pages/about');
+    return;
+  }
+
+  if (url === 'contact') {
+    loadPage(response, 'pages/contact');
+    return;
+  }
+
+  console.error('404: Not Found');
+  loadPage(response);
+});
